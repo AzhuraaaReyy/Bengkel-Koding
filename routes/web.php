5c127988cd23be7db22a176929ceba34ea01b372
AzhuraaaReyy <azhurareyy@gmail.com>
2025-04-23 00:34:47 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ObatController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DokterController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('layouts.login');
 });
 
+
 Route::get('/login', [AuthController::class, 'form'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
+Route::get('/register', [RegisterController::class, 'formRegister'])->name('daftar');
+Route::post('/register', [RegisterController::class, 'register']);
 Route::middleware(['auth', 'role:dokter'])->group(function () {
     Route::get('/obat', [ObatController::class, 'index'])->name('obat.index');
     Route::get('/obat/{id}/edit', [ObatController::class, 'edit'])->name('obat.edit');
